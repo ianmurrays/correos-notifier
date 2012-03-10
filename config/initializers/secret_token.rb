f700be8e46aa051eaff1bda6b93fc846e6a5ee34
Ian Murray <ianmurrays@gmail.com>
2012-03-10 20:53:51 -0300

    Initial commit with nokogiri and heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Correos::Application.config.secret_token = 'c226918e4ffae4f429ffd0b692a10b118e09286e0a59c8c477d2d8d84e6ea2653d0ba1fa289000238ff8b176b9edeea3e2cfa36f14fc3b2a751527ab50125697'
