f50911b3f70dd32912c4dabccc744d4ed3cf7c01
Ian Murray <ianmurrays@gmail.com>
2012-03-10 23:25:07 -0300

    Adds configuration for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,14 @@ Correos::Application.configure do
   config.action_mailer.default_url_options = {
     :host => "correos.linkworks.cl"
   }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
