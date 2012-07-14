012167d1666fd72339d00dafa0f43e56d21355f1
Ian Murray <ianmurrays@gmail.com>
2012-07-14 13:40:51 -0400

    Changes smtp settings to those of gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,22 +70,23 @@ Correos::Application.configure do
     :host => "correos.linkworks.cl"
   }
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.sendmail_settings = {
-    :location       => '/usr/sbin/sendmail',
-    :arguments      => '-i -t'
-  }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com'
+  # config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.sendmail_settings = {
+  #   :location       => '/usr/sbin/sendmail',
+  #   :arguments      => '-i -t'
   # }
-  # ActionMailer::Base.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'mail.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => 'correos@linkworks.cl',
+    :password       => 'correos2012',
+    :domain         => 'linkworks.cl',
+    :enable_starttls_auto => true
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
